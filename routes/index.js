46d670d80b434096fdbc1539f78252ef2f58567e
MrMPerial <christopher.mitchell26@gmail.com>
2017-09-15 10:28:43 -0500

    Added a third Endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const audio = require('./audio');
 const video = require('./video');
-//const example = require('./example');
+const example = require('./example');
 
 router.get('/', (req, res) => {
   res.status(200).send('So far, so good!');
@@ -10,6 +10,6 @@ router.get('/', (req, res) => {
 
 router.use('/audio', audio);
 router.use('/video', video);
-//router.use('/example', example);
+router.use('/example', example);
 
 module.exports = router;
