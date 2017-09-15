fe62e94a575b9304f934c5f9ad2f0d20837d6df2
MrMPerial <christopher.mitchell26@gmail.com>
2017-09-15 08:42:19 -0500

    Created Video

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const audio = require('./audio');
-//const video = require('./video');
+const video = require('./video');
 //const example = require('./example');
 
 router.get('/', (req, res) => {
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 });
 
 router.use('/audio', audio);
-//router.use('/video', video);
+router.use('/video', video);
 //router.use('/example', example);
 
 module.exports = router;
